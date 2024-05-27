c3c673385b4c0005809216daf6a393aad66ac619
Michael Epstein <mepstein19@gmail.com>
2024-05-26 22:46:52 -0400

    add eks variables

--- eks-cluster/cluster.tf
+++ eks-cluster/cluster.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_cluster" "k8s-demo-eks-cluster" {
-  name     = var.cluster_name
+  name     = "${var.cluster_name}-cluster"
   version  = var.kubernetes_version
   role_arn = aws_iam_role.k8s-demo-cluster.arn
 
@@ -17,29 +17,25 @@ resource "aws_eks_cluster" "k8s-demo-eks-cluster" {
 
 resource "aws_eks_node_group" "k8s-demo-eks-node-group" {
   cluster_name    = aws_eks_cluster.k8s-demo-eks-cluster.name
-  node_group_name = var.cluster_name
-  node_role_arn   = aws_iam_role.k8s-demo-node.arn
+  node_group_name = "${var.cluster_name}-mng"
+  node_role_arn   = aws_iam_role.k8s-demo-nodes.arn
   subnet_ids      = aws_subnet.k8s-demo-subnet.*.id
+
+  ami_type = var.ami_type
+  disk_size = var.disk_size
+  instance_types = var.instance_types
 
   scaling_config {
     desired_size = 1
-    max_size     = 3
+    max_size     = 2
     min_size     = 1
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
-    aws_iam_role_policy_attachment.k8s-demo-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.k8s-demo-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.k8s-demo-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_read_only,
   ]
-}
-
-output "cluster_url" {
-  value = aws_eks_cluster.k8s-demo-eks-cluster.endpoint
-}
-
-output "cluster_ca" {
-  value = aws_eks_cluster.k8s-demo-eks-cluster.certificate_authority[0].data
 }
