2f65de91f0100a5da1af9066bd0bdf92ccc9b29a
Michael Epstein <mepstein19@gmail.com>
2024-05-26 22:48:04 -0400

    add tls+local provider

--- eks-cluster/providers.tf
+++ eks-cluster/providers.tf
@@ -4,9 +4,19 @@ terraform {
       source = "hashicorp/aws"
       version = "5.51.1"
     }
+    tls = {
+      source = "hashicorp/tls"
+      version = "4.0.5"
+    }
+    local= {
+      source = "hashicorp/local"
+      version = "2.5.1"
+    }
   }
 }
 
 provider "aws" {
     region = var.region
 }
+provider "tls" {}
+provider "local" {}
