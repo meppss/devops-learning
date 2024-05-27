515441e04e97002eb16ab0700fbb40ba0ca63177
Michael Epstein <mepstein19@gmail.com>
2024-05-26 22:47:25 -0400

    add instance profile + keypair

--- eks-cluster/ec2.tf
+++ eks-cluster/ec2.tf
@@ -1,10 +1,28 @@
+resource "tls_private_key" "rsa_private_key" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+resource "aws_key_pair" "tf_keypair" {
+  key_name = var.key_pair_name
+  public_key = tls_private_key.rsa_private_key.public_key_openssh
+}
+
+resource "local_file" "tf_key" {
+    content = tls_private_key.rsa_private_key.private_key_pem
+    filename = "mongodb-instance.pem"
+  
+}
+
 resource "aws_instance" "mongodb_server" {
-    ami = data.aws_ami.amazon_linux
+    # AMI specified below maps to: amzn2-ami-hvm-2.0.20230612.0-x86_64-gp2 |  ami-0030e9fc5c777545a  
+    ami = "ami-0030e9fc5c777545a"
     instance_type = "t2.micro"
+    key_name = aws_key_pair.tf_keypair.key_name
     subnet_id = aws_subnet.k8s-demo-subnet[0].id
     vpc_security_group_ids = [aws_security_group.k8s-demo-sg.id]
     associate_public_ip_address = true
     user_data = data.template_file.user_data.rendered
+    iam_instance_profile = aws_iam_instance_profile.mongodb_instance_profile.name
 
     tags = merge(var.tags,
     {
