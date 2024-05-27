ff9d276230b457e47ee55c4878ec09a1439ff791
Michael Epstein <mepstein19@gmail.com>
2024-05-26 22:48:09 -0400

    add s3

---
+++ eks-cluster/s3.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "mongodb_backups" {
+    bucket = "batcomputer-backups-aladin01"
+}
+
+resource "aws_s3_bucket_public_access_block" "mongodb_backups_public_access" {
+  bucket = aws_s3_bucket.mongodb_backups.id
+
+  block_public_acls = false
+  block_public_policy = false
+}
+
+resource "aws_s3_bucket_policy" "mongodb_backups_policy" {
+  bucket = aws_s3_bucket.mongodb_backups.id
+  policy = data.aws_iam_policy_document.allow_access_from_public_readonly.json
+}
