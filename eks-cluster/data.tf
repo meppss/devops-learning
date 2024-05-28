01a0a95fd19279f20f95ffa16b63ee01f3f4555b
Michael Epstein <mepstein19@gmail.com>
2024-05-28 13:13:32 -0400

    update data.tf

--- eks-cluster/data.tf
+++ eks-cluster/data.tf
@@ -2,13 +2,8 @@ data "aws_region" "current" {}
 
 data "aws_availability_zones" "available" {}
 
-data "cloudinit_config" "user_data" {
-  gzip          = true
-  base64_encode = true
-  part {
-    content_type = "text/cloud-config"
-    content = templatefile("${path.module}/scripts/cloud_init.yml")
-  }
+data "template_file" "user_data" {
+  template = file("${path.module}/scripts/cloud_init.cfg")
 }
 
 data "aws_iam_policy_document" "allow_access_from_public_readonly" {
