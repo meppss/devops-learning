59793ac9787ac42b4b0d51f044e48b1bf29355c2
Michael Epstein <mepstein19@gmail.com>
2024-05-28 13:16:25 -0400

    update providers.tf

--- eks-cluster/providers.tf
+++ eks-cluster/providers.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/aws"
       version = "5.51.1"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.30.0"
+    }
     tls = {
       source = "hashicorp/tls"
       version = "4.0.5"
@@ -12,11 +16,24 @@ terraform {
       source = "hashicorp/local"
       version = "2.5.1"
     }
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
+    }
   }
 }
 
 provider "aws" {
     region = var.region
 }
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "my-context"
+
+  host                   = aws_eks_cluster.k8s-demo-eks-cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.eks-cluster-auth.token
+  cluster_ca_certificate = base64decode(aws_eks_cluster.k8s-demo-eks-cluster.certificate_authority.0.data)
+}
 provider "tls" {}
 provider "local" {}
+provider "template" {}
