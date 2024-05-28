8491241e1b520c9b841f7e380f20679f3265a858
Michael Epstein <mepstein19@gmail.com>
2024-05-27 22:36:05 -0400

    modify s3 public permissions

--- eks-cluster/s3.tf
+++ eks-cluster/s3.tf
@@ -6,7 +6,8 @@ resource "aws_s3_bucket_public_access_block" "mongodb_backups_public_access" {
   bucket = aws_s3_bucket.mongodb_backups.id
 
   block_public_acls = false
-  block_public_policy = false
+  ignore_public_acls = true
+  
 }
 
 resource "aws_s3_bucket_policy" "mongodb_backups_policy" {
