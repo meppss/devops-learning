845b6849cd3052de81c1019b3142ece8685af794
Michael Epstein <mepstein19@gmail.com>
2024-06-06 16:33:36 -0400

    fix vnet name

--- terraform/1-vnet.tf
+++ terraform/1-vnet.tf
@@ -1,6 +1,6 @@
 # Define vnet resource
 resource "azurerm_virtual_network" "vnet" {
-  name                    = "${var.vnet_name}-vnet" 
+  name                    = "${var.environment}-vnet" 
   location                = var.location
   resource_group_name     = azurerm_resource_group.sc_rg.name
   address_space           = var.network_address_space
