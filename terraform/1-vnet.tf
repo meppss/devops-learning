476f4fe73f63727bcf88793ae589a9e5018466c2
Michael Epstein <mepstein19@gmail.com>
2024-06-10 15:25:39 -0400

    refactor dynamic nsg rules to separate resource using for_each

--- terraform/1-vnet.tf
+++ terraform/1-vnet.tf
@@ -69,24 +69,23 @@ resource "azurerm_subnet_network_security_group_association" "vm-sga" {
 
 #NSG Resources
 resource "azurerm_network_security_group" "nsg" {
-  name = "${var.environment}-sg"
+  name = "${var.environment}-nsg"
   location = var.location
   resource_group_name = azurerm_resource_group.sc_rg.name
   tags = var.tags
-  
-  dynamic security_rule {
-    for_each = var.sg_rules
-    content {
-      name                       = security_rule.value["name"]
-      priority                   = security_rule.value["priority"]
-      direction                  = security_rule.value["direction"]
-      access                     = security_rule.value["access"]
-      protocol                   = security_rule.value["protocol"]
-      source_port_range          = security_rule.value["source_port_range"]
-      destination_port_range     = security_rule.value["destination_port_range"]
-      source_address_prefixes     = security_rule.value["source_address_prefixes"]
-      destination_address_prefix  = security_rule.value["destination_address_prefix"]
-    }
-  }
 }
 
+resource "azurerm_network_security_rule" "nsg_rules" {
+  for_each                    = local.nsg_rules
+  name                        = each.value.name
+  priority                    = each.value.priority
+  direction                   = each.value.direction
+  access                      = each.value.access
+  protocol                    = each.value.protocol
+  source_port_range           = each.value.source_port_range
+  destination_port_range      = each.value.destination_port_range
+  source_address_prefix       = each.value.source_address_prefix
+  destination_address_prefix  = each.value.destination_address_prefix
+  resource_group_name         = azurerm_resource_group.sc_rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name 
+}
