a931132a932f2e7a3fdf7b3aeffe3818c250d5a9
Michael Epstein <mepstein19@gmail.com>
2024-06-10 09:55:57 -0400

    add outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,19 @@
-output "virtual_network_gateway_ip" {
-  value = azurerm_virtual_network_gateway.prisma_sc_vpng.ip_configuration
+output "azure_bgp_ip" {
+  value = azurerm_virtual_network_gateway.prisma_sc_vpng.ip_configuration[0]
+}
+
+output "azure_bgp_asn" {
+  value = azurerm_virtual_network_gateway.prisma_sc_vpng.bgp_settings[0]
+}
+
+/* output "azure_tunnel_ip" {
+  value = azurerm_virtual_network_gateway.prisma_sc_vpng.ip_configuration.private_ip_address
+} */
+
+output "azure_vm_ip" {
+  value = azurerm_linux_virtual_machine.ubuntu.*.private_ip_address
+}
+
+output "shared_key" {
+  value = random_password.psk_gen.result
 }
