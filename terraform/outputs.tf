75aae53113c6396ae320d011efeeefa79db6cfda
Michael Epstein <mepstein19@gmail.com>
2024-06-10 22:57:01 -0400

    remove unneeded outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,6 @@
-output "azure_bgp_ip" {
-  value = azurerm_virtual_network_gateway.prisma_sc_vpng.ip_configuration[0]
-}
-
 output "azure_bgp_asn" {
   value = azurerm_virtual_network_gateway.prisma_sc_vpng.bgp_settings[0]
 }
-
-/* output "azure_tunnel_ip" {
-  value = azurerm_virtual_network_gateway.prisma_sc_vpng.ip_configuration.private_ip_address
-} */
 
 output "azure_vm_ip" {
   value = azurerm_linux_virtual_machine.ubuntu.*.private_ip_address
